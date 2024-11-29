65cb8fa74d23fe426a94beb48321e699f912682a
ahesmat <ahmed.moh.esmat@gmail.com>
2024-11-29 05:32:20 +0000

    Final

---
+++ Create-EC2-Instance-With-Jenkins/login-to-created-instance.sh
@@ -0,0 +1,2 @@
+chmod 400 jenkins-key.pem
+ssh -o StrictHostKeyChecking=no -i jenkins-key.pem ubuntu@3.86.218.85
--- Create-EC2-Instance-With-Jenkins/main.tf
+++ Create-EC2-Instance-With-Jenkins/main.tf
@@ -55,13 +55,18 @@ resource "aws_instance" "jenkins_server" {
   }
 provisioner "remote-exec" {
   inline=[
+  " echo '###############Installing Java###############################'",
+  "sudo apt update -y",
   "sudo apt install openjdk-21-jdk -y",
+  " echo '###############Installing Jenkins#############################'",
   "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
   "echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]' https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
   "sudo apt update -y",
   "sudo apt install jenkins -y",
   "sudo systemctl start jenkins && sudo systemctl enable jenkins",
-  "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+  "echo '###############Printing Adminstrator Password##########################'",
+  "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+  "echo '#######################################################################'"
   ]
 }
  connection {
