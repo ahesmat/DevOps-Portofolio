eaf8fde77786fb0430bf3abdb2857a24be8a770d
ahesmat <ahmed.moh.esmat@gmail.com>
2024-12-04 09:09:21 -0800

    Update bootstrap.sh

--- Create-K8s-Cluster/bootstrap.sh
+++ Create-K8s-Cluster/bootstrap.sh
@@ -1,1 +1,16 @@
+# Installing Terraform and AWS CLI
+echo "#########################################Installing Terraform#################################################"
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+echo "#######################################Installing AWS########################################################"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -r awscliv2.zip
+rm -r aws
+terraform init
 
