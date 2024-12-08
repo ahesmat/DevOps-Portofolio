dbe2fa89aa05e0a00da74fddfccd3e461f2975ce
ahmed <ahmed.moh.esmat@gmail.com>
2024-12-08 01:57:32 +0000

    adding VPC file

--- Create-K8s-Cluster/bootstrap.sh
+++ Create-K8s-Cluster/bootstrap.sh
@@ -5,7 +5,7 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
-echo "#######################################Installing AWS########################################################"
+echo "#######################################Installing AWS CLIi ########################################################"
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
