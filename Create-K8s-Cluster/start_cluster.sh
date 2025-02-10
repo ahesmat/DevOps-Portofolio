824ed6feb6a80079276bc8c82f7249d825318126
ahesmat <ahmed.moh.esmat@gmail.com>
2025-02-10 20:32:41 +0000

    Organizing the ANSIBLE playbooks

--- Create-K8s-Cluster/start_cluster.sh
+++ Create-K8s-Cluster/start_cluster.sh
@@ -3,6 +3,7 @@ terraform apply --auto-approve
 terraform apply -target module.worker-alb-listener --auto-approve 
 terraform apply -target module.master-alb-listener --auto-approve
 generate_ansible_inventory.sh
+cd ansible-playbooks
 ansible-playbook -i ansible_inventory.ini mount-ebs-volume-idenpodant.yaml
 ansible-playbook -i ansible_inventory.ini install_docker.yml
 ansible-playbook -i ansible_inventory.ini install_k8s.yml
