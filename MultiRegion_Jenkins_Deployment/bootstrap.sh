798d4124fa84e4179eee33d0eb5e15156ebdb02a
ahmed <ahmed.moh.esmat@gmail.com>
2025-02-22 05:54:49 +0000

    ALB

--- MultiRegion_Jenkins_Deployment/bootstrap.sh
+++ MultiRegion_Jenkins_Deployment/bootstrap.sh
@@ -21,3 +21,8 @@ sudo apt update -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+echo "########################################Installing Boto3####################################################"
+
+sudo apt install python3-pip -y
+pip3 install boto3 --user
