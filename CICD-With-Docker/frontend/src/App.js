e5c285d470f976935016fb4fe90bb4b2013c7726
ahmed <ahmed.moh.esmat@gmail.com>
2025-03-11 15:32:18 +0000

    CICD-Docker-Done

---
+++ CICD-With-Docker/frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+	  Hello There, this is Ahmed Esmat
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          "I'm glad to see you in my website"
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
